2d4a0c7c90f68b2d42807fc91a742ccb71e82a56
LiuKozlovets <51923745+LiuKozlovets@users.noreply.github.com>
2021-07-27 13:18:11 +0300

    initial

---
+++ src/js/fetchCountries.js
@@ -0,0 +1,13 @@
+const fetchCountries = (searchQuery) => {
+    return fetch(`https://restcountries.eu/rest/v2/name/${searchQuery}`)
+    .then(response => {
+      if (searchQuery) {
+        return response.json();
+      } else {
+        return [];
+      }
+    })
+    .catch(error => console.log(error));
+}
+
+export default fetchCountries;
